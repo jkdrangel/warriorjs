0a2f241bed208c8a7c57ad3ba01a218ced93d4f0
jkdrangel <jkdrangel@hotmail.com>
2018-05-15 10:54:41 -0300

    :tada: Initial commit

---
+++ jkdrangel-beginner/Player.js
@@ -0,0 +1,9 @@
+class Player {
+  playTurn(warrior) {
+    if (warrior.feel().isEmpty()) {
+      if (warrior.health() < 20) {
+        warrior.rest();
+      } else warrior.walk();
+    } else warrior.attack();
+  }
+}
